ee940ca94e3026bed80219728f4e2da85b2e91d1
JacobKoewler <jdkoewler22@gmail.com>
2025-12-17 20:00:55 -0500

    Updated Data Wrangling with read_sheet
    Changed data input to read_sheet.

--- Final_Project_Data_Wrangling_jdk.R
+++ Final_Project_Data_Wrangling_jdk.R
@@ -4,14 +4,19 @@ library(rvest)
 library(ggplot2)
 library(dplyr)
 library(readr)
+library(googlesheets4)
 
 # Step 2: Import Raw Data
-Raw_Statistics <- read.csv("C:/College/GitHub Repos/Sec-1-FP-JacobKoewler-LiamBengston-YeheCui/stats (1).csv")
+gs4_deauth()
+web_Raw <- read_sheet(
+  ss = "https://docs.google.com/spreadsheets/d/1aQGzS0Epzus4ZRMdT7VMvGddumRQdRsXYek41cmyLLU/edit?gid=884425286#gid=884425286",
+  n_max = 1113
+)
 
 # Step 3: Split Name into First and Last Name
-Raw_Statistics_Separated <- Raw_Statistics %>% 
+Raw_Statistics_Separated <- web_Raw %>% 
   separate_wider_delim( # Separate one name column into two (First and Last)
-    cols = `last_name..first_name`, # original column
+    cols = `last_name, first_name`, # original column
     delim = ",", # delimiter
     names = c("Last_Name", "First_Name"), # new column names
   )
@@ -24,7 +29,7 @@ Raw_Statistics_Names <- Raw_Statistics_Separated %>% # Turn names from "Colon, B
     Last_Name, # Last name column
     sep = " ") # Separate names with space
 
-# Step 4: Rename Columns
+# Step 5: Rename Columns
 Raw_Statistics_Renamed <- Raw_Statistics_Names %>% # rename columns for ease of use for those who aren't as familiar with baseball statistics
   rename("Games_Pitched" = p_game,
          "Innings_Pitched" = p_formatted_ip,
@@ -34,16 +39,16 @@ Raw_Statistics_Renamed <- Raw_Statistics_Names %>% # rename columns for ease of 
          "On_Base_Percentage" = on_base_percent)
 
 
-# Step 5: Calculate WHIP
+# Step 6: Calculate WHIP
 Raw_Statistics_WHIP <- Raw_Statistics_Renamed %>%
   mutate( # Calculate WHIP using mutate to create a new column
     "WHIP" = (walk + hit)/(Innings_Pitched) # use PEMDAS to ensure proper calculation
 )
 
-# Step 6: Remove Extra Columns
+# Step 7: Remove Extra Columns
 Cleaned_ERA_Statistics <- Raw_Statistics_WHIP %>% # remove extra columns so only useful data remains
   select(Name, Strikeout_Percentage, Walk_Percentage, On_Base_Percentage, WHIP, ERA)
 
-# Step 7: Round WHIP
+# Step 8: Round WHIP
 Cleaned_ERA_Statistics$WHIP <- round(Cleaned_ERA_Statistics$WHIP, digits = 2)
 
